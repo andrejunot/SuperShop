7b66b93e4a3019c0995fa0e6ce4fbc2886e3f4fb
andrejunot <andrejunot@hotmail.com>
2024-09-04 10:15:52 +0100

    Confirm orders

--- SuperShop/Data/Entities/Order.cs
+++ SuperShop/Data/Entities/Order.cs
@@ -27,10 +27,18 @@ namespace SuperShop.Data.Entities
         public IEnumerable<OrderDetail> Items { get; set; }
 
 
-        [DisplayFormat(DataFormatString = "{0:N2")]
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public int Lines => Items == null ? 0 : Items.Count();
+
+
+        [DisplayFormat(DataFormatString = "{0:N2}")]
         public double Quantity => Items == null ? 0 : Items.Sum(i => i.Quantity);
 
-        [DisplayFormat(DataFormatString = "{0:C2")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
         public decimal Value => Items == null ? 0 : Items.Sum(i => i.Value);
+
+        [Display(Name = "Order date")]
+        [DisplayFormat(DataFormatString = "{0:mm/dd/yyyy HH:mm}", ApplyFormatInEditMode = false)]
+        public DateTime? OrderDateLocal => this.OrderDate == null ? null : this.OrderDate.ToLocalTime();
     }
 }
